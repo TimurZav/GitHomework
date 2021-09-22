6aac650fd60b66aa129c8b664b95d67b2f3b7b45
Timur <timurka.zavyalov@mail.ru>
2021-09-22 12:34:58 +0300

    CHANGES

--- GitHome/Git.py
+++ GitHome/Git.py
@@ -1,3 +1,6 @@
 print("Hi there")
 print('Hello')
 print("bye")
+m = [1, 2, 3, 4]
+for i in m:
+    print(i)
--- GitHome2/Git2.py
+++ GitHome2/Git2.py
@@ -1,3 +1,6 @@
 print("Hello, World")
 print("Hello")
 print("Hi")
+m = [3124, 23, 34]
+for i in m:
+    print(i)
